e3670449ae099699d92d881e97b809f56019629c
JulianRivers <juliancamilorifo@ufps.edu.co>
2023-08-10 17:21:30 -0500

    first commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0});
+}
