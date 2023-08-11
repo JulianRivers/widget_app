6e16e9837c2378d0e39a69f99661f9e14b4ceb83
JulianRivers <juliancamilorifo@ufps.edu.co>
2023-08-11 13:16:35 -0500

    home screen

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -10,20 +10,18 @@ const colorList = <Color>[
   Colors.purpleAccent,
   Colors.cyan,
   Colors.orange,
-  
-
 ];
 
 class AppTheme {
   final int selectedColor;
 
   AppTheme({this.selectedColor = 0})
-  :assert(selectedColor >= 0,'Selected color must be greater than 0'),
-  assert(selectedColor < colorList.length, 
-  'Selected color must be less or equal than ${colorList.length - 1}');
+      : assert(selectedColor >= 0, 'Selected color must be greater than 0'),
+        assert(selectedColor < colorList.length,
+            'Selected color must be less or equal than ${colorList.length - 1}');
 
   ThemeData getTheme() => ThemeData(
-    useMaterial3: true,
-    colorSchemeSeed: colorList[selectedColor],
-  );
+      useMaterial3: true,
+      colorSchemeSeed: colorList[selectedColor],
+      appBarTheme: const AppBarTheme(centerTitle: false));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widget_app/config/theme/app_theme.dart';
+import 'package:widget_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,10 +14,8 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme(selectedColor: 5).getTheme(),
-      home:  const Scaffold(
-        body: Center(
-          child:  Text('Hola'),
-        ),
+      home: const Scaffold(
+        body: Center(child: HomeScreen()),
       ),
     );
   }
