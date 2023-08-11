90504be28e51cd6eb6d026c93ab56db9552868a1
Julian Riveros <juliancamilorifo@ufps.edu.co>
2023-08-11 12:24:40 -0500

    avances

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -4,10 +4,26 @@ const colorList = <Color>[
   Colors.blue,
   Colors.teal,
   Colors.green,
+  Colors.pink,
+  Colors.pinkAccent,
+  Colors.purple,
+  Colors.purpleAccent,
+  Colors.cyan,
+  Colors.orange,
+  
+
 ];
 
 class AppTheme {
   final int selectedColor;
 
-  AppTheme({this.selectedColor = 0});
+  AppTheme({this.selectedColor = 0})
+  :assert(selectedColor >= 0,'Selected color must be greater than 0'),
+  assert(selectedColor < colorList.length, 
+  'Selected color must be less or equal than ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    colorSchemeSeed: colorList[selectedColor],
+  );
 }
